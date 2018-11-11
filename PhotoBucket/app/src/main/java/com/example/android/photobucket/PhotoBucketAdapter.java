dbf7c6916613053aaa46161bba74d13fa4a6850b
senanoc <senanoc@gmail.com>
2018-11-11 13:26:36 +0000

    Added PhotoBucket Adapter

---
+++ PhotoBucket/app/src/main/java/com/example/android/photobucket/PhotoBucketAdapter.java
@@ -0,0 +1,89 @@
+package com.example.android.photobucket;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhotoBucketAdapter {
+
+    private List<DocumentSnapshot> mPhotoBucketSnapshots = new ArrayList<>();
+
+
+    public PhotoBucketAdapter() {
+        CollectionReference photoBucketRef = FirebaseFirestore.getInstance().collection(Constants.COLLECTION_PATH);
+
+        photoBucketRef.orderBy(Constants.KEY_CREATED, Query.Direction.DESCENDING).limit(50)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(Constants.TAG, "Listening failed!");
+                            return;
+                        }
+                        mPhotoBucketSnapshots = documentSnapshots.getDocuments();
+                        notifyDataSetChanged();
+
+                    }
+                });
+    }
+
+    @NonNull
+    @Override
+    public PhotoBucketViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.photobucket_itemview, parent, false);
+        return new PhotoBucketViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PhotoBucketViewHolder movieQuoteViewHolder, int i) {
+        DocumentSnapshot ds = mPhotoBucketSnapshots.get(i);
+        String quote = (String) ds.get(Constants.KEY_CAPTION);
+        String movie = (String) ds.get(Constants.KEY_URL);
+        movieQuoteViewHolder.mCaptionTextView.setText(caption);
+        movieQuoteViewHolder.mImageURLTextView.setText(ImageURL);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPhotoBucketSnapshots.size();
+    }
+
+    class PhotoBucketViewHolder extends RecyclerView.ViewHolder {
+        private TextView mCaptionTextView;
+        private TextView mImageURLTextView;
+
+        public PhotoBucketViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mCaptionTextView = itemView.findViewById(R.id.itemview_caption);
+            mImageURLTextView = itemView.findViewById(R.id.itenview_imageurl);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    DocumentSnapshot ds = mPhotoBucketSnapshots.get(getAdapterPosition());
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, PhotoBucketDetailActivity.class);
+                    intent.putExtra(Constants.EXTRA_DOC_ID, ds.getId());
+                    context.startActivity(intent);
+                }
+            });
+
+        }
+    }
+}
