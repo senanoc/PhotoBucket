fd9b9ceb899a629972d73e5b6e08ab4bf33460b9
senanoc <senanoc@gmail.com>
2018-11-11 13:03:31 +0000

    Added constants file

---
+++ PhotoBucket/app/src/main/java/com/example/android/photobucket/Constants.java
@@ -0,0 +1,12 @@
+package com.example.android.photobucket;
+
+public class Constants {
+
+    public static final String TAG = "PB";
+    public static final String COLLECTION_PATH = "photobucket";
+    public static final String KEY_CAPTION = "caption";
+    public static final String KEY_URL = "imageUrl";
+    public static final String KEY_CREATED= "created";
+    public static final String EXTRA_DOC_ID = "document_id";
+    
+}
