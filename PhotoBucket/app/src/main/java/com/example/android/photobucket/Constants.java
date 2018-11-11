e051ddfcd33099a3d503fdd272aa5c0cd1452b65
senanoc <senanoc@gmail.com>
2018-11-11 13:41:59 +0000

    Added PB itemview xml & fixed adapter

--- PhotoBucket/app/src/main/java/com/example/android/photobucket/Constants.java
+++ PhotoBucket/app/src/main/java/com/example/android/photobucket/Constants.java
@@ -8,5 +8,5 @@ public class Constants {
     public static final String KEY_URL = "imageUrl";
     public static final String KEY_CREATED= "created";
     public static final String EXTRA_DOC_ID = "document_id";
-    
+
 }
